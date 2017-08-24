0fd8755648b73b35dda7b87b253b9f970873ab3b
Ningty <2460763383@qq.com>
2017-08-24 09:54:49 +0800

    2017.8.24 Signed-off-by: Ningty <2460763383@qq.com>

---
+++ js/NTY-js/case.js
@@ -0,0 +1,9 @@
+var review=document.getElementById("review");
+var zhezhao=document.getElementById("zhezhao");
+var zhezhaoImg=document.getElementById("zhezhao-img");
+review.onclick=function(){
+	zhezhao.style.display="block";
+};
+zhezhaoImg.onclick=function(){
+	zhezhao.style.display="none";
+}
