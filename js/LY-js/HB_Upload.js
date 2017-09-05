f836b07c9942df1d916d5361e724f9971aa8cb15
HDDLDR <yz1329090770@qq.com>
2017-09-05 10:41:17 +0800

    轮播图分页..

---
+++ js/LY-js/HB_Upload.js
@@ -0,0 +1,253 @@
+	var Num=window.location.href.slice(-1);
+	console.log(Num);
+
+	//头部信息
+	//对应的图集
+	var Cons=[
+					{imgSrc:'../../images/LY-img/HomeBanner_header_1.jpg',					//头像图片
+					meTitle:'华丽梦幻的卡塔尔王室婚礼',				//大标题
+					inTime:'发布时间：2015年4月2日 22:11',        //发布时间
+					 msg:'堪称壮观的卡塔尔王室婚礼，婚礼在一个小型宫殿中举行，有700多位来宾出席了婚礼。优雅精致的布置，加上粉红色的婚礼色彩，充满了浪漫氛围。',					//简介
+				     proMsg:'<p>作品信息<br>婚礼地点:<a href="#">卡塔尔</a><br>环境: <a href="#">室内</a><br>风格: <a href="#">华丽 梦幻</a> <br>色彩:<a href="#">粉红色</a></p>',
+				     products:['婚礼','真实婚礼','国外婚礼','粉红色','华丽','梦幻','室内','王室'],
+				     pictures:[
+				     			'../../images/LY-img/HB_list_1 (1).jpg','../../images/LY-img/HB_list_1 (1).jpg','../../images/LY-img/HB_list_1 (2).jpg',
+				     			'../../images/LY-img/HB_list_1 (3).jpg','../../images/LY-img/HB_list_1 (4).jpg',
+				     			'../../images/LY-img/HB_list_1 (5).jpg','../../images/LY-img/HB_list_1 (6).jpg',
+				     			'../../images/LY-img/HB_list_1 (7).jpg','../../images/LY-img/HB_list_1 (8).jpg',
+				     			'../../images/LY-img/HB_list_1 (9).jpg','../../images/LY-img/HB_list_1 (10).jpg',
+				     			'../../images/LY-img/HB_list_1 (11).jpg','../../images/LY-img/HB_list_1 (12).jpg',
+				     			'../../images/LY-img/HB_list_1 (13).jpg','../../images/LY-img/HB_list_1 (14).jpg',
+				     			'../../images/LY-img/HB_list_1 (15).jpg','../../images/LY-img/HB_list_1 (16).jpg',
+				     			'../../images/LY-img/HB_list_1 (17).jpg','../../images/LY-img/HB_list_1 (18).jpg',
+				     			'../../images/LY-img/HB_list_1 (19).jpg','../../images/LY-img/HB_list_1 (20).jpg',
+				     			'../../images/LY-img/HB_list_1 (21).jpg','../../images/LY-img/HB_list_1 (22).jpg',
+				     			'../../images/LY-img/HB_list_1 (23).jpg','../../images/LY-img/HB_list_1 (24).jpg',
+				     			'../../images/LY-img/HB_list_1 (25).jpg','../../images/LY-img/HB_list_1 (26).jpg',
+				     	]
+
+				},{imgSrc:'../../images/LY-img/HomeBanner_header_2.jpg',					//头像图片
+					meTitle:'Goodness of forest——水晶雨主题婚礼',				//大标题
+					inTime:'发布时间：2015年3月2日 8:49',        //发布时间
+					 msg:'神秘静谧的森林，晨雾流动，犹如精灵舞动着轻纱，他与她相遇在雨中，朦胧薄雾，雨点仿佛是颗颗水晶；秋千轻曳，他承诺用一生为她遮风挡雨。烟雾弥漫，水晶莹莹，小鹿误入水晶林，寻觅到一生最爱的女神，唯美的森林系场景总是以一种梦幻形式呈现。把他们爱情的情感元素水晶作为婚礼的切入点，舞台上璀璨精致的水晶赋予婚礼奢华氛围，留影区充满浪漫的灵性熏染，每个场景都沁入人心。',					//简介
+				     proMsg:'<p>作品信息<br>婚礼地点:<a href="#">卡塔尔</a><br>环境:<a href="#">室内</a><br>风格: <a href="#">华丽 梦幻</a> <br>色彩: <a href="#">粉红色</a></p>',
+				     products:['森林婚礼','華麗邂逅婚礼策划执行总监','佛山婚礼策划','梦幻紫色婚礼','水晶雨'],
+				     pictures:[
+				     			'../../images/LY-img/HB_list_2 (1).jpg','../../images/LY-img/HB_list_2 (1).jpg','../../images/LY-img/HB_list_2 (2).jpg',
+				     			'../../images/LY-img/HB_list_2 (3).jpg','../../images/LY-img/HB_list_2 (4).jpg',
+				     			'../../images/LY-img/HB_list_2 (5).jpg','../../images/LY-img/HB_list_2 (6).jpg',
+				     			'../../images/LY-img/HB_list_2 (7).jpg','../../images/LY-img/HB_list_2 (8).jpg',
+				     			'../../images/LY-img/HB_list_2 (9).jpg','../../images/LY-img/HB_list_2 (10).jpg',
+				     			'../../images/LY-img/HB_list_2 (11).jpg','../../images/LY-img/HB_list_2 (12).jpg',
+				     			'../../images/LY-img/HB_list_2 (13).jpg','../../images/LY-img/HB_list_2 (14).jpg',
+				     			'../../images/LY-img/HB_list_2 (15).jpg','../../images/LY-img/HB_list_2 (16).jpg',
+				     			'../../images/LY-img/HB_list_2 (17).jpg'
+				     	]
+
+				},{imgSrc:'../../images/LY-img/HomeBanner_header_3.jpg',					//头像图片
+					meTitle:'Goodness of forest——水晶雨主题婚礼',				//大标题
+					inTime:'发布时间：2015年3月2日 8:49',        //发布时间
+					 msg:'神秘静谧的森林，晨雾流动，犹如精灵舞动着轻纱，他与她相遇在雨中，朦胧薄雾，雨点仿佛是颗颗水晶；秋千轻曳，他承诺用一生为她遮风挡雨。烟雾弥漫，水晶莹莹，小鹿误入水晶林，寻觅到一生最爱的女神，唯美的森林系场景总是以一种梦幻形式呈现。把他们爱情的情感元素水晶作为婚礼的切入点，舞台上璀璨精致的水晶赋予婚礼奢华氛围，留影区充满浪漫的灵性熏染，每个场景都沁入人心。',					//简介
+				     proMsg:'<p>作品信息<br>婚礼地点:<a href="#">卡塔尔</a><br>环境:<a href="#">室内</a><br>风格: <a href="#">华丽 梦幻</a> <br>色彩: <a href="#">粉红色</a></p>',
+				     products:['森林婚礼','華麗邂逅婚礼策划执行总监','佛山婚礼策划','梦幻紫色婚礼','水晶雨'],
+				     pictures:[
+				     			'../../images/LY-img/HB_list_3 (1).jpg','../../images/LY-img/HB_list_3 (1).jpg','../../images/LY-img/HB_list_3 (2).jpg',
+				     			'../../images/LY-img/HB_list_3 (3).jpg','../../images/LY-img/HB_list_3 (4).jpg',
+				     			'../../images/LY-img/HB_list_3 (5).jpg','../../images/LY-img/HB_list_3 (6).jpg',
+				     			'../../images/LY-img/HB_list_3 (7).jpg','../../images/LY-img/HB_list_3 (8).jpg',
+				     			'../../images/LY-img/HB_list_3 (9).jpg','../../images/LY-img/HB_list_3 (10).jpg',
+				     			'../../images/LY-img/HB_list_3 (11).jpg','../../images/LY-img/HB_list_3 (12).jpg',
+				     			'../../images/LY-img/HB_list_3 (13).jpg','../../images/LY-img/HB_list_3 (14).jpg',
+				     			'../../images/LY-img/HB_list_3 (15).jpg','../../images/LY-img/HB_list_3 (16).jpg'
+				     	]
+
+				},{imgSrc:'../../images/LY-img/HomeBanner_header_4.jpg',					//头像图片
+					meTitle:'Goodness of forest——水晶雨主题婚礼',				//大标题
+					inTime:'发布时间：2015年3月2日 8:49',        //发布时间
+					 msg:'神秘静谧的森林，晨雾流动，犹如精灵舞动着轻纱，他与她相遇在雨中，朦胧薄雾，雨点仿佛是颗颗水晶；秋千轻曳，他承诺用一生为她遮风挡雨。烟雾弥漫，水晶莹莹，小鹿误入水晶林，寻觅到一生最爱的女神，唯美的森林系场景总是以一种梦幻形式呈现。把他们爱情的情感元素水晶作为婚礼的切入点，舞台上璀璨精致的水晶赋予婚礼奢华氛围，留影区充满浪漫的灵性熏染，每个场景都沁入人心。',					//简介
+				     proMsg:'<p>作品信息<br>婚礼地点:<a href="#">卡塔尔</a><br>环境:<a href="#">室内</a><br>风格: <a href="#">华丽 梦幻</a> <br>色彩: <a href="#">粉红色</a></p>',
+				     products:['森林婚礼','華麗邂逅婚礼策划执行总监','佛山婚礼策划','梦幻紫色婚礼','水晶雨'],
+				     pictures:[
+				     			'../../images/LY-img/HB_list_4 (1).jpg','../../images/LY-img/HB_list_4 (1).jpg','../../images/LY-img/HB_list_4 (2).jpg',
+				     			'../../images/LY-img/HB_list_4 (3).jpg','../../images/LY-img/HB_list_4 (4).jpg',
+				     			'../../images/LY-img/HB_list_4 (5).jpg','../../images/LY-img/HB_list_4 (6).jpg',
+				     			'../../images/LY-img/HB_list_4 (7).jpg','../../images/LY-img/HB_list_4 (8).jpg',
+				     			'../../images/LY-img/HB_list_4 (9).jpg','../../images/LY-img/HB_list_4 (10).jpg',
+				     			'../../images/LY-img/HB_list_4 (11).jpg','../../images/LY-img/HB_list_4 (12).jpg',
+				     			'../../images/LY-img/HB_list_4 (13).jpg','../../images/LY-img/HB_list_4 (14).jpg',
+				     			'../../images/LY-img/HB_list_4 (15).jpg','../../images/LY-img/HB_list_4 (16).jpg',
+				     			'../../images/LY-img/HB_list_4 (17).jpg','../../images/LY-img/HB_list_4 (18).jpg',
+				     			'../../images/LY-img/HB_list_4 (19).jpg'
+				     	]
+
+				},{imgSrc:'../../images/LY-img/HomeBanner_header_5.jpg',					//头像图片
+					meTitle:'Goodness of forest——水晶雨主题婚礼',				//大标题
+					inTime:'发布时间：2015年3月2日 8:49',        //发布时间
+					 msg:'神秘静谧的森林，晨雾流动，犹如精灵舞动着轻纱，他与她相遇在雨中，朦胧薄雾，雨点仿佛是颗颗水晶；秋千轻曳，他承诺用一生为她遮风挡雨。烟雾弥漫，水晶莹莹，小鹿误入水晶林，寻觅到一生最爱的女神，唯美的森林系场景总是以一种梦幻形式呈现。把他们爱情的情感元素水晶作为婚礼的切入点，舞台上璀璨精致的水晶赋予婚礼奢华氛围，留影区充满浪漫的灵性熏染，每个场景都沁入人心。',					//简介
+				     proMsg:'<p>作品信息<br>婚礼地点:<a href="#">卡塔尔</a><br>环境:<a href="#">室内</a><br>风格: <a href="#">华丽 梦幻</a> <br>色彩: <a href="#">粉红色</a></p>',
+				     products:['森林婚礼','華麗邂逅婚礼策划执行总监','佛山婚礼策划','梦幻紫色婚礼','水晶雨'],
+				     pictures:[
+				     			'../../images/LY-img/HB_list_5 (1).jpg','../../images/LY-img/HB_list_5 (1).jpg','../../images/LY-img/HB_list_5 (2).jpg',
+				     			'../../images/LY-img/HB_list_5 (3).jpg','../../images/LY-img/HB_list_5 (4).jpg',
+				     			'../../images/LY-img/HB_list_5 (5).jpg','../../images/LY-img/HB_list_5 (6).jpg',
+				     			'../../images/LY-img/HB_list_5 (7).jpg','../../images/LY-img/HB_list_5 (8).jpg'
+				     	]
+
+				},{imgSrc:'../../images/LY-img/HomeBanner_header_6.jpg',					//头像图片
+					meTitle:'Goodness of forest——水晶雨主题婚礼',				//大标题
+					inTime:'发布时间：2015年3月2日 8:49',        //发布时间
+					 msg:'神秘静谧的森林，晨雾流动，犹如精灵舞动着轻纱，他与她相遇在雨中，朦胧薄雾，雨点仿佛是颗颗水晶；秋千轻曳，他承诺用一生为她遮风挡雨。烟雾弥漫，水晶莹莹，小鹿误入水晶林，寻觅到一生最爱的女神，唯美的森林系场景总是以一种梦幻形式呈现。把他们爱情的情感元素水晶作为婚礼的切入点，舞台上璀璨精致的水晶赋予婚礼奢华氛围，留影区充满浪漫的灵性熏染，每个场景都沁入人心。',					//简介
+				     proMsg:'<p>作品信息<br>婚礼地点:<a href="#">卡塔尔</a><br>环境:<a href="#">室内</a><br>风格: <a href="#">华丽 梦幻</a> <br>色彩: <a href="#">粉红色</a></p>',
+				     products:['森林婚礼','華麗邂逅婚礼策划执行总监','佛山婚礼策划','梦幻紫色婚礼','水晶雨'],
+				     pictures:[
+				     			'../../images/LY-img/HB_list_6 (1).jpg','../../images/LY-img/HB_list_6 (1).jpg','../../images/LY-img/HB_list_6 (2).jpg',
+				     			'../../images/LY-img/HB_list_6 (3).jpg','../../images/LY-img/HB_list_6 (4).jpg',
+				     			'../../images/LY-img/HB_list_6 (5).jpg','../../images/LY-img/HB_list_6 (6).jpg',
+				     			'../../images/LY-img/HB_list_6 (7).jpg','../../images/LY-img/HB_list_6 (8).jpg',
+				     			'../../images/LY-img/HB_list_6 (9).jpg','../../images/LY-img/HB_list_6 (10).jpg',
+				     			'../../images/LY-img/HB_list_6 (11).jpg','../../images/LY-img/HB_list_6 (12).jpg',
+				     			'../../images/LY-img/HB_list_6 (13).jpg','../../images/LY-img/HB_list_6 (14).jpg',
+				     			'../../images/LY-img/HB_list_6 (15).jpg','../../images/LY-img/HB_list_6 (16).jpg',
+				     			'../../images/LY-img/HB_list_6 (17).jpg','../../images/LY-img/HB_list_6 (18).jpg',
+				     			'../../images/LY-img/HB_list_6 (19).jpg','../../images/LY-img/HB_list_6 (20).jpg'
+				     	]
+
+				},{imgSrc:'../../images/LY-img/HomeBanner_header_7.jpg',					//头像图片
+					meTitle:'Goodness of forest——水晶雨主题婚礼',				//大标题
+					inTime:'发布时间：2015年3月2日 8:49',        //发布时间
+					 msg:'神秘静谧的森林，晨雾流动，犹如精灵舞动着轻纱，他与她相遇在雨中，朦胧薄雾，雨点仿佛是颗颗水晶；秋千轻曳，他承诺用一生为她遮风挡雨。烟雾弥漫，水晶莹莹，小鹿误入水晶林，寻觅到一生最爱的女神，唯美的森林系场景总是以一种梦幻形式呈现。把他们爱情的情感元素水晶作为婚礼的切入点，舞台上璀璨精致的水晶赋予婚礼奢华氛围，留影区充满浪漫的灵性熏染，每个场景都沁入人心。',					//简介
+				     proMsg:'<p>作品信息<br>婚礼地点:<a href="#">卡塔尔</a><br>环境:<a href="#">室内</a><br>风格: <a href="#">华丽 梦幻</a> <br>色彩: <a href="#">粉红色</a></p>',
+				     products:['森林婚礼','華麗邂逅婚礼策划执行总监','佛山婚礼策划','梦幻紫色婚礼','水晶雨'],
+				     pictures:[
+				     			'../../images/LY-img/HB_list_7 (1).jpg','../../images/LY-img/HB_list_7 (1).jpg','../../images/LY-img/HB_list_7 (2).jpg',
+				     			'../../images/LY-img/HB_list_7 (3).jpg','../../images/LY-img/HB_list_7 (4).jpg',
+				     			'../../images/LY-img/HB_list_7 (5).jpg','../../images/LY-img/HB_list_7 (6).jpg',
+				     			'../../images/LY-img/HB_list_7 (7).jpg','../../images/LY-img/HB_list_7 (8).jpg',
+				     			'../../images/LY-img/HB_list_7 (9).jpg','../../images/LY-img/HB_list_7 (10).jpg',
+				     			'../../images/LY-img/HB_list_7 (11).jpg','../../images/LY-img/HB_list_7 (12).jpg',
+				     			'../../images/LY-img/HB_list_7 (13).jpg','../../images/LY-img/HB_list_7 (14).jpg',
+				     			'../../images/LY-img/HB_list_7 (15).jpg'
+				     	]
+
+				},
+
+					];
+
+//   对应的id生成对应的内容
+ console.log(Cons[Num-1]);
+ $(".HB").css({background:'url("../../images/LY-img/HB_list_1%20(5).jpg")no-repeat',backgroundSize:'100% 120%'})
+  $(".HB_header").append('<img src="'+Cons[Num-1].imgSrc+'" alt="加载失败"><div class="HBmsgTT"><div><h3>'+Cons[Num-1].meTitle+'</h3><p>'+Cons[Num-1].inTime+'</p></div>');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	// <!-- -----------------------瀑布流------------------------- -->
+ //随机数
+		    function Random(min,max){
+		        return parseInt(Math.random()*((max+1)-min)+min);
+		    }
+		//创建一个数组来保存图片
+		var ImgArr= Cons[Num-1].pictures;
+		 // console.log(ImgArr);
+			//console.log(ImgArr[0].src);
+			var i = 0;//定义一个变量
+			//动态生成图片标签，添加到Li里面
+			function create(){//封装一个函数来创建
+				//创建一个div标签
+				var oDiv = document.createElement("div");
+				var oImg = new Image();//新建一个图片对象
+				//0%12 0/12=0 余0 1/12=0余1 2/12=0余2 12/12=1余0 13/12=1余1
+				oImg.alt = '加载失败了~';
+				oImg.src = ImgArr[i%ImgArr.length];//把数组里面图片的路径赋值给img
+
+				oDiv.style.cssText = "opacity:0;transform:scale(0.5)";
+				oDiv.appendChild(oImg);//把图片标签放到div里面
+				//把div放到高度最小的li里面
+				getList($("#myUpload ul li")).append(oDiv);
+				(function(oImg){
+					setTimeout(function(){
+						oDiv.style.cssText = "opacity:1;transform:scale(1);";
+					},10);
+				})(oImg)//立马执行
+			}
+
+
+
+
+			//create();//调用函数
+			//alert(ImgArr.length);
+			//封装一个函数来获取高度最小的li
+			function getList(obj){
+				var length = obj.length;//定义一个变量保存li的列数
+				var h = Infinity;//每一列的高度都可以无限高
+				var oLi;//定义一个变量来保存符合条件的li列返回出去
+				for (var i=0;i<length ;i++ )
+				{
+					//循环获取每一列的高度来和无限高h作对比 若果小于h加给这个元素添加eq(i)  jq插件里面具体获取某一个元素
+					if (obj.eq(i).height() < h)
+					{
+						h = obj.eq(i).height();
+						oLi = obj.eq(i);
+					}
+				}
+				return oLi;
+			}
+			//封装一个函数来循环动态添加多个图片
+			var sum;
+			function upload(){//自定的
+				i++;
+				if(i<12){
+					for(;i<12;i++){
+						create();
+					}
+				}else{
+					sum = i;
+					for (;i<sum+3 ;i++ )
+					{
+						create();
+					}
+				}
+			}
+			upload();
+
+			//判断滚动条的高度，然后动态添加
+			var scrollH = '';//文档高度
+			var srollT = '';//滚动条高度
+			var _height = $(window).height();//获取可视区域的高度
+			 var numStart=0;
+				window.onscroll=function(){
+					myScroll(numStart)
+				};
+
+				function myScroll(num){
+					$('#LY_insHeader').css({background:'rgba(255,255,255,.6)'});
+					scrollH = document.body.scrollHeight||document.documentElement.scrollHeight;//文档高度
+					srollT = document.body.scrollTop||document.documentElement.scrollTop;
+					if(srollT<=0){
+							$('#LY_insHeader').css({background:'#fff'});
+					}
+					//看得见的可视区域高度加上看不见的滚动条高度之和如果大于文档的高度的话就再添加图片
+					if(_height + srollT + 50 > scrollH){  upload();  }
+					// console.log($('ul li div').length);
+					if($('ul li div').length>num+56) { window.onscroll=null;}
+				};
